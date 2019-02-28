7be9356e0a004cf4300f4ddf840dfb3252e8f747
Ollie Nye <ollie@nyemail.co.uk>
2019-02-28 21:27:13 +0000

    PUSHHHH

--- slideshow.rb
+++ slideshow.rb
@@ -6,28 +6,30 @@ class Slideshow
     @slide_count = 1
 
     @slides << in_slides.delete_at(0)
-    available_slides = in_slides
-    while available_slides.count.positive?
-      available_slides = order(available_slides.first, available_slides)
+    # order(in_slides.first, in_slides.reject { |s| s == in_slides.first })
+
+    @available_slides = in_slides
+    while @available_slides.count > 2
+      order(@slides.last)
     end
 
     output
   end
 
-  def order(start, available_slides)
-    next_slide_index = find_next_slide(start, available_slides)
-    next_slide = available_slides[next_slide_index]
+  def order(start)
+    next_slide_index = find_next_slide(start)
+    next_slide = @available_slides[next_slide_index]
     @slides << next_slide
     @slide_count += 1
-    puts "#{available_slides.count} remaining"
-    available_slides.delete_at(next_slide_index)
-    available_slides
+    puts "#{@available_slides.count} remaining"
+    # order(next_slide, available_slides.delete_at(next_slide_index)) unless available_slides.count < 2
+    @available_slides.delete_at(next_slide_index)
   end
 
-  def find_next_slide(slide, available_slides)
+  def find_next_slide(slide)
     best_score = 0
     best_slide = nil
-    available_slides[0..5].each_with_index do |check_slide, index|
+    @available_slides[0..5].each_with_index do |check_slide, index|
       score = slide.score check_slide
       best_slide = index if score > best_score
     end
