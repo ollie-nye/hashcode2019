5c88cde27a53809ccd50b268695a4dca56a3764c
Simon Fish <si@mon.fish>
2019-02-28 19:05:12 +0000

    fix

--- main.rb
+++ main.rb
@@ -15,6 +15,7 @@ photos = Parser.new('a_example.txt').photos
 #   slides << Slide.new(photo) if photo.orientation == 'H'
 # end
 
+<<<<<<< HEAD
 # v_photos = photos.select { |p| p.orientation == 'V' }
 
 # v_photos.each_with_index do |photo, index|
@@ -46,3 +47,19 @@ puts 'sa'
 pp sa
 
 pp s.score sa
+=======
+v_photos = photos.select { |p| p.orientation == 'V' }
+
+def optimal_photo(photos)
+  starting_photo = photos.first
+  best_score = 0
+  chosen_slide = nil
+  photos[1..].each do |current_photo|
+    current_slide = Slide.new([starting_photo, current_photo])
+    chosen_slide = current_slide if current_slide.score > best_score?
+  end
+  return chosen_slide
+end
+
+pp optimal_photo(v_photos)
+>>>>>>> fix it please
