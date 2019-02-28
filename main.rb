cb3ac5f8579500eff727c3e1abcbb2a79575e63d
Simon Fish <si@mon.fish>
2019-02-28 18:16:13 +0000

    Compare photos

--- main.rb
+++ main.rb
@@ -1,4 +1,8 @@
 require_relative './parser'
 require_relative './photo'
 
-print Parser.new('a_example.txt').photos
+photos = Parser.new('a_example.txt').photos
+print photos.first.inspect
+print photos[1].inspect
+puts photos.first.tags_shared_with(photos[1])
+puts photos.first.tags_different_from(photos[1])
--- photo.rb
+++ photo.rb
@@ -1,7 +1,21 @@
 class Photo
+  attr_reader :tags, :orientation
+
   def initialize(input)
     values = input.split
     @orientation = values[0]
     @tags = values[2..]
+  end
+
+  def tags_shared_with(photo)
+    (self.tags & photo.tags).size
+  end
+
+  def tags_different_from(photo)
+    [(self.tags - photo.tags).size, (photo.tags - self.tags).size].min
+  end
+
+  def compatible_with(photo)
+    self.orientation == photo.orientation
   end
 end
