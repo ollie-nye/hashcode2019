16cc6815f28ab6749a68a8177a039588caa6f0ec
Ollie Nye <ollie@nyemail.co.uk>
2019-02-28 18:02:38 +0000

    Add photos to parser

--- parser.rb
+++ parser.rb
@@ -1,10 +1,18 @@
+require './photo.rb'
+
 class Parser
-  attr_accessor :file, :number_of_photos
+  attr_accessor :file, :number_of_photos, :photos
 
   def initialize(filename)
+    first = true
     @file = File.open(filename, 'r')
-    @number_of_photos = file.gets
+    @photos = []
+    @file.each do |line|
+      if first
+        @number_of_photos = line
+        first = false
+      end
+      @photos << Photo.new(line)
+    end
   end
-
-  
 end
